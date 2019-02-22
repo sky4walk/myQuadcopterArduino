d733f29b1007a5f5b22a3b1588722caad6bc3f7d
Andre Betz <git@andrebetz.de>
2019-02-22 22:41:58 +0100

    added avrdude

---
+++ MultiWii/installFW.bat
@@ -0,0 +1,9 @@
+REM Arduino über USB Bus anschliessen
+REM %AVRPATH%\listComPorts.exe aufrufen um COM Port zu bestimmen
+REM erhaltenen COM Port anstatt COM7 eintragen
+
+SET AVRPATH=avrdude
+SET COMPORT=COM8
+
+%AVRPATH%\avrdude.exe -C%AVRPATH%\avrdude.conf -v -patmega328p -carduino -P%COMPORT% -b57600 -D -Uflash:w:FW\FW.ino.hex:i 
+
